b0bae44f9ac0768d475c25b0dac627418ad73029
Valente Ramirez <37411799+valentermz@users.noreply.github.com>
2018-05-16 20:14:04 +0200

    values.txt now consists of [t_0,...,d_3,U,V]

--- data/raw-input/extract-values.py
+++ data/raw-input/extract-values.py
@@ -1,6 +1,6 @@
 """This code generates the file 'data.txt'.
 Each line in 'raw-input-depurated.txt' contains two 7-element lists of eigenvalues l, m.
-This script scans through his file, and converts them into a list [t0, .. d3, Lambda].
+This script scans through his file, and converts them into a list [t0, .. d3, U, V].
 The output is appended to 'values.txt'
 
 Note: The fixed points are numbered p_0,...,p_6, where p_4, p_5, p_6 are on the invariant line.
@@ -32,18 +32,21 @@ for line in lines:
         dict[line] = 1
 
         # For each special line we extract t, d, L
-        l = eval(line)[0]
-        m = eval(line)[1]
+        u = eval(line)[0]
+        v = eval(line)[1]
 
         # t and d are saved in a list
-        t = list(l[k] + m[k] for k in range(4))
-        d = list(l[k] * m[k] for k in range(4))
+        t = list(u[k] + v[k] for k in range(4))
+        d = list(u[k] * v[k] for k in range(4))
 
-        # L is saved as its numerator and denominator
-        L = [m[4] * m[5] * m[6], l[4] * l[5] * l[6]]
+        # U is the product of tangential eigenvalues
+        U = u[4] * u[5] * u[6]
+
+        # V is the product of normal eigenvalues
+        V = v[4] * v[5] * v[6]
 
         # Combine t, d and L to a string
-        string = str(t + d + [L])
+        string = str(t + d + [U] + [V])
 
         # Append to output_file
         output_file.write(string + '\n')
